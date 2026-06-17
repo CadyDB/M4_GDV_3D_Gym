4c313702d89445a296249d4e78d66557193a4826
CadyDB <40341@ma-web.nl>
2026-06-17 12:01:33 +0200

    les5 camera

---
+++ Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.Cinemachine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    [SerializeField] private CinemachineCamera followCam;
+    [SerializeField] private CinemachineCamera overviewCam;
+    [SerializeField] private InputActionAsset inputAsset;
+    private InputAction switchAction;
+    private bool overviewActive = false;
+
+    void Awake()
+    {
+        switchAction = inputAsset.FindActionMap("Player1").FindAction("SwitchCamera");
+    }
+
+    void OnEnable()
+    {
+        inputAsset.FindActionMap("Player1").Enable();
+    }
+    
+    void OnDisable()
+    {
+        inputAsset.FindActionMap("Player1").Disable();
+    }
+    void Update()
+    {
+        if (switchAction.WasPressedThisFrame())
+        {
+            overviewActive = !overviewActive;
+            followCam.Priority = overviewActive ? 0 : 10;
+            overviewCam.Priority = overviewActive ? 10 : 0;
+        } 
+    }
+}
